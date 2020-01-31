789d16aff7bcd798d50b7e0b3faa0f6103180717
naomi-rc <naomi.cat182128@gmail.com>
2020-01-30 20:43:36 -0500

    switched exports to using api - no duplicate endpoints

--- socialapp-functions/functions/index.js
+++ socialapp-functions/functions/index.js
@@ -1,15 +1,14 @@
 const functions = require('firebase-functions');
-var admin = require("firebase-admin");
-var serviceAccount = require("../key/admin.json");
+var admin = require('firebase-admin');
+var serviceAccount = require('../key/admin.json');
 const express = require('express');
-
-
 const app = express();
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://socialapp-6b1c8.firebaseio.com"
 });
-
+//admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -18,43 +17,37 @@ admin.initializeApp({
   response.send("Hello world");
  });
 
- exports.getScreams = functions.https.onRequest((req, resp) => {
-    admin.firestore().collection("screams").get().then((data) => {
+ app.get("/scream", (req, resp) => {
+    admin.firestore().collection("screams").orderBy('createdAt', 'desc').get().then((data) => {
         let screams = [];
-        data.forEach(doc => screams.push(doc.data()));
+        data.forEach(doc => screams.push({
+            screamId: doc.id,
+            userHandle: doc.data().userHandle,
+            body: doc.data().body,
+            createdAt: doc.data().createdAt
+        }));
         return resp.json(screams);
     })
     .catch((err) => console.error(err));
  });
 
- exports.getScreamUser = functions.https.onRequest((req, resp) =>{
-    admin.firestore().collection('screams').get().then((data) =>{
-        let screamUsers = [];
-        data.forEach(doc => screamUsers.push(doc.data()["userHandle"]));
-        return resp.json(screamUsers);
+ app.post("/scream", (req, resp) => {
+    const newScream = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: new Date().toISOString()
+    };
+
+    admin.firestore().collection("screams")
+    .add(newScream)
+    .then(doc => {
+        resp.json({message: `Doc ${doc.id} added successfully.`});
     })
-    .catch((err) => console.error(err));
-
+    .catch(err => {
+        resp.status(500).json({error: 'Something went wrong.'});
+        console.error(err);
+    });
  });
+ 
 
- exports.createScream = functions.https.onRequest((req, resp) => {
-    if(req.method != "POST"){
-        return resp.status(400).json({error: 'Bad request - method not allowed'});
-    }
-    
-     const newScream = {
-         body: req.body.body,
-         userHandle: req.body.userHandle,
-         createdAt: admin.firestore.Timestamp.fromDate(new Date())
-     };
-
-     admin.firestore().collection("screams")
-     .add(newScream)
-     .then(doc => {
-         resp.json({message: `Doc ${doc.id} added successfully.`});
-     })
-     .catch(err => {
-         resp.status(500).json({error: 'Something went wrong.'});
-         console.error(err);
-     });
- });
+ exports.api = functions.https.onRequest(app);
