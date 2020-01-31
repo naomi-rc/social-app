66344f7ced9b634935436b255c1fd36f18fdedbd
naomi-rc <naomi.cat182128@gmail.com>
2020-01-30 21:20:11 -0500

    added signup route

--- socialapp-functions/functions/index.js
+++ socialapp-functions/functions/index.js
@@ -1,13 +1,27 @@
 const functions = require('firebase-functions');
 var admin = require('firebase-admin');
-var serviceAccount = require('../key/admin.json');
-const express = require('express');
-const app = express();
+//var serviceAccount = require('../key/admin.json');
+const app = require('express')();
+admin.initializeApp();
 
-admin.initializeApp({
+const config = {
+    apiKey: "AIzaSyCG2pkbmqYAhBeiQFxNrJSVNRsgSVFkULQ",
+    authDomain: "socialapp-6b1c8.firebaseapp.com",
+    databaseURL: "https://socialapp-6b1c8.firebaseio.com",
+    projectId: "socialapp-6b1c8",
+    storageBucket: "socialapp-6b1c8.appspot.com",
+    messagingSenderId: "825869421390",
+    appId: "1:825869421390:web:7f4e2b7bf2f27391f02c1f",
+    measurementId: "G-DPXWBHX84Y"
+  };
+
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
+/*admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://socialapp-6b1c8.firebaseio.com"
-});
+});*/
 //admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
@@ -49,5 +63,25 @@ admin.initializeApp({
     });
  });
  
+
+ app.post("/signup", (req, resp) =>{
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    };
+
+    // TODO : validate data
+
+    firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password).then(data => {
+        return resp.status(201).json({message: `User ${data.user.uid} signed up successfully.`}); //201: ressource created
+    })
+    .catch(err => {
+        console.error(err);
+        return resp.status(500).json({error: `Error signing up user: ${err.code}`});
+    });
+
+ });
 
  exports.api = functions.https.onRequest(app);
